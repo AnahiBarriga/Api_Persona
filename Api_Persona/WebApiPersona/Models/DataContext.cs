838fc0eb4089f199da5ebc05b6faab9bf0454ab8
AnahiBarriga <a2019119382@estudiantes.upsa.edu.bo>
2021-01-19 19:36:24 -0600

    Se creo People Controller

---
+++ Api_Persona/WebApiPersona/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiPersona.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+         public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebApiPersona.Models.Person> People { get; set; }
+    }
+}
